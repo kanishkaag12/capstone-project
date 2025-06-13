a23fbbf2e006d50d8aae4f4b2583c1821f5a464f
Kanishka Agarwal <kanishkaagarwal201@gmail.com>
2025-06-13 16:50:03 +0530

    I am committing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
+import Navigation from './Components/Navigation';
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        HomePage
-      </div>
+    <> 
+        HomePage HII
     </>
   )
 }
---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,14 @@
+function Navigation() {
+    <div>
+        <img src="/Logo.svg"/>
+        <ul>
+            <li><a href="" >Home</a></li>
+            <li><a href="" >About</a></li>
+            <li><a href="" >Reservations</a></li>
+            <li><a href="" >Order Online</a></li>
+            <li><a href="" >Login</a></li>
+        </ul>
+    </div>
+}
+
+export default Navigation;
